9f69bddd38651371d17205cf95d0f666e398c5c2
Todd Campbell <73364217+ToddCampbellDotCom@users.noreply.github.com>
2020-12-11 00:10:27 -0800

    Initial commit

---
+++ Domain/Value.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Domain
+{
+    public class Value
+    {
+        // Code first.  Create classes then db
+
+        public int Id { get; set; }     // Id automatically PK with Entity
+
+        public string Name { get; set; }
+    }
+}
